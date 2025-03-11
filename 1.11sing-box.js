2a350646ce610a5fff8ac78f369c988b43c7ac3e
baiyian <luolin3377@gmail.com>
2025-03-11 21:20:57 +0800

    Update 1.11sing-box.js

--- 1.11sing-box.js
+++ 1.11sing-box.js
@@ -16,7 +16,7 @@ let proxies = await produceArtifact({
 config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
-  if (['all', 'all-auto'].includes(i.tag)) {
+  if (['🚀 节点选择', '♻️ 自动选择'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies))
   }
   if (['🇭🇰 香港节点', '🔯 香港自动'].includes(i.tag)) {
