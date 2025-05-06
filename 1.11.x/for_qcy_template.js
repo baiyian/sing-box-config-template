824496b55e36788635611595eccb70776fffb1e7
baiyian <luolin3377@gmail.com>
2025-05-06 20:39:20 +0800

    Update for_qcy_template.js

--- 1.11.x/for_qcy_template.js
+++ 1.11.x/for_qcy_template.js
@@ -16,7 +16,7 @@ let proxies = await produceArtifact({
 config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
-  if (['GLOBAL', '♻️ 自动选择','🐸 手动切换'].includes(i.tag)) {
+  if (['♻️ 自动选择','🐸 手动切换'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies))
   }
   if (['🇭🇰 香港节点', '🔯 香港自动'].includes(i.tag)) {
